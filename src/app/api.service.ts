bb852291a0fe4d5e1800c9d4e4bab85e0bb7c34a
Robkata44 <118105712+Robkata44@users.noreply.github.com>
2022-12-05 15:23:54 +0200

    Add files via upload
    Components

---
+++ src/app/api.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
+import { ITheme } from './interfaces/theme';
+import { IPost } from './interfaces/post';
+
+const apiUrl = environment.apiURL;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  loadThemes() {
+    return this.httpClient.get<ITheme[]>(`${apiUrl}/themes`)
+  };
+
+  loadPosts(limit?: number) {
+    return this.httpClient.get<IPost[]>(`${apiUrl}/posts${limit ? `?limit=${limit}` : ""}`);
+  };
+
+};
